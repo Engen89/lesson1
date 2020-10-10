aba051eef7a615acfe62909a5f733428fd8a2f0d
Александр <alexprait@gmail.com>
2020-10-10 12:43:52 +0300

    Homework 5 course 2

---
+++ src/main/java/lesson5Coirse2/Main.java
@@ -0,0 +1,11 @@
+package lesson5Coirse2;
+
+public class Main {
+    static  final int SIZE = 10000000;
+    static  final int HALF = SIZE / 2;
+    float[] arr = new float[SIZE];
+
+    public static void main(String[] args) {
+
+    }
+}
