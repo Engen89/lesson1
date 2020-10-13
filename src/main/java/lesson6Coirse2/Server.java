708fbc1a82b3d0ccfa12cd9927556b62e13ae3e0
Александр <alexprait@gmail.com>
2020-10-13 23:22:57 +0300

    Homework 6 course 2 the end

--- src/main/java/lesson6Coirse2/Server.java
+++ src/main/java/lesson6Coirse2/Server.java
@@ -1,7 +1,55 @@
 package lesson6Coirse2;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
     public static void main(String[] args) {
+        Socket clientSocket = null;
+        Scanner scanner = new Scanner(System.in);
 
+        try(ServerSocket serverSocket = new ServerSocket(8888)) {
+            System.out.println("Server started");
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getRemoteSocketAddress());
+            DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            Thread threadReader = new Thread(() ->{
+                try {
+                    while (true){
+                        outputStream.writeUTF(scanner.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            threadReader.setDaemon(true);
+            threadReader.start();
+
+            while (true) {
+                String str = inputStream.readUTF();
+                if (str.equals("/close")) {
+                    System.out.println("client left the server");
+                    outputStream.writeUTF("/close");
+                    break;
+                } else {
+                    System.out.println("Client: " + str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
